803b7a2dce455dd5c2a549aeb6c7b5e2adf15b78
Islam walied <islam.walied96@gmail.com>
2023-09-21 15:32:22 +0300

    adjust category routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+\App\Helpers\Routes\RouteHelper::includeRouteFiles(__DIR__ . '/api/v1');
